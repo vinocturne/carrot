cdb2ec31159010b7287eaaefd70091ea1f16ec25
Ghunhee Shin <hago@rocketstaff.com>
2022-05-31 13:03:18 +0900

    Feat(stream 컴포넌트) : "추가 stream 컴포넌트"
    - 실시간 방송 컴포넌트(index, 방송 시작, 방송 화면)
    - Upload의 상품 이름 설정 필드 구현

--- pages/stream/create.tsx
+++ pages/stream/create.tsx
@@ -1,28 +1,8 @@
 import type { NextPage } from "next";
 
-const Upload: NextPage = () => {
+const Create: NextPage = () => {
     return (
-        <div className="px-4 space-y-5 py-16">
-            <div>
-                <label className="w-full cursor-pointer text-gray-600 hover:text-orange-500 hover:border-orange-500 flex items-center justify-center border-2 border-dashed border-gray-300 h-48 rounded-md">
-                    <svg
-                        className="h-12 w-12"
-                        stroke="currentColor"
-                        fill="none"
-                        viewBox="0 0 48 48"
-                        aria-hidden="true"
-                    >
-                        <path
-                            d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02"
-                            strokeWidth={2}
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                        />
-                    </svg>
-
-                    <input type="file" className="hidden" />
-                </label>
-            </div>
+        <div className="py-10 space-y-5 px-4">
             <div>
                 <label
                     className="mb-1 block text-sm font-medium text-gray-700"
@@ -39,6 +19,7 @@ const Upload: NextPage = () => {
                     />
                 </div>
             </div>
+            <div></div>
             <div>
                 <label
                     className="mb-1 block text-sm font-medium text-gray-700"
@@ -75,10 +56,10 @@ const Upload: NextPage = () => {
                 />
             </div>
             <button className="mt-4 w-full bg-orange-500 hover:bg-orange-600 text-white py-2 px-4 border border-transparent rounded-md shadow-sm font-medium focus:ring-2 focus:ring-offset-2 focus:ring-orange-500 focus:outline-none">
-                Upload product
+                Go Live!!
             </button>
         </div>
     );
 };
 
-export default Upload;
+export default Create;
