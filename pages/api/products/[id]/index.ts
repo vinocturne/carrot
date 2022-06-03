1ea8d4a98dbaffa23c9e4e03eb8f10e1333b65f1
Ghunhee Shin <hago@rocketstaff.com>
2022-06-03 18:23:54 +0900

    Feat(Liked) : "추가 Liked"
    좋아요 추가 기능을 위한 db작업 및 좋아요 선택 시 해당 테이블에 레코드를 추가하고 삭제하는 로직을 추가하였습니다.
    현재 즉각 반응하지는 않고 있지만 optimistic UI를 통해 서버를 통하지 않고 바로 UI가 반응할 수 있도록 변경 예정입니다.

--- pages/api/products/[id]/index.ts
+++ pages/api/products/[id]/index.ts
@@ -7,7 +7,10 @@ async function handler(
     req: NextApiRequest,
     res: NextApiResponse<ResponseType>
 ) {
-    const { id } = req.query;
+    const {
+        query: { id },
+        session: { user },
+    } = req;
     const product = await client.product.findUnique({
         where: {
             id: +id.toString(),
@@ -41,6 +44,17 @@ async function handler(
     });
     console.log(relatedProducts);
     console.log(terms);
-    res.json({ ok: true, product, relatedProducts });
+    const isLiked = Boolean(
+        await client.fav.findFirst({
+            where: {
+                productId: product?.id,
+                userId: user?.id,
+            },
+            select: {
+                id: true,
+            },
+        })
+    );
+    res.json({ ok: true, product, isLiked, relatedProducts });
 }
 export default withApiSession(withHandler({ methods: ["GET"], handler }));
