776990658e728efd8e3492a84e3ce7e3be6dccab
Ghunhee Shin <hago@rocketstaff.com>
2022-06-13 14:23:02 +0900

    Feat(Stream) : "추가 스트리밍 구현"
    cloudflare로부터 스트리밍에 필요한 Key와 url을 받아와 스트리밍을 진행하는 유저에게 표시해주고, 그 이외의 유저에게는 streamingId만 공유하여 라이브를 시청할 수 있도록 구현하였습니다.
    lifecycle을 요청하면 현재 방송 여부와 시청자 수 등을 받아올 수 있어 해당 기능은 후에 챌린지로 추가해볼 예정입니다.

--- pages/streams/[id].tsx
+++ pages/streams/[id].tsx
@@ -72,7 +72,14 @@ const LiveDetail: NextPage = () => {
     return (
         <Layout canGoBack>
             <div className="py-10 px-4  space-y-4">
-                <div className="w-full rounded-md shadow-sm bg-slate-300 aspect-video" />
+                {data?.stream.cloudflareId ? (
+                    <iframe
+                        src={`https://iframe.videodelivery.net/${data?.stream?.cloudflareId}`}
+                        className="w-full aspect-video rounded-md shadow-sm"
+                        allow="accelerometer; gyroscope; autoplay; encrypted-media; picture-in-picture;"
+                        allowFullScreen={true}
+                    ></iframe>
+                ) : null}
                 <div className="mt-5">
                     <h1 className="text-3xl font-bold text-gray-900">
                         {data?.stream?.name}
@@ -83,6 +90,21 @@ const LiveDetail: NextPage = () => {
                     <p className=" my-6 text-gray-700">
                         {data?.stream?.description}
                     </p>
+                    <div className="bg-orange-300 flex flex-col p-5 rounded-md overflow-x-scroll space-y-3">
+                        <span>Stream Keys (secret)</span>
+                        <span className="text-gray-600">
+                            <span className="font-medium text-gray-800">
+                                URL
+                            </span>
+                            :{data?.stream.cloudflareUrl}
+                        </span>
+                        <span className="text-gray-600">
+                            <span className="font-medium text-gray-800">
+                                Key
+                            </span>
+                            :{data?.stream.cloudflareKey}
+                        </span>
+                    </div>
                 </div>
                 <div>
                     <h2 className="text-2xl font-bold text-gray-900">
