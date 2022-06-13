dbde2a361fac3f3fed66abc69c098a6702f82ae3
Ghunhee Shin <hago@rocketstaff.com>
2022-06-13 16:17:17 +0900

    Feat(middleware, dynamic import) : "추가 미들웨어 및 Dynamic import"
    middleware를 통해 페이지 이동시에 useUser에서 진행하는 세션 확인을 미드웨어에서 할 수 있도록 수정.
    dynamic import의 기능을 확인하고 옵션 활용.

---
+++ components/bs.tsx
@@ -0,0 +1,4 @@
+console.log("bs component");
+export default function Bs() {
+    return <h3>Hello</h3>;
+}
---
+++ pages/_middleware.ts
@@ -0,0 +1,14 @@
+import type { NextRequest, NextFetchEvent } from "next/server";
+import { NextResponse } from "next/server";
+export function middleware(req: NextRequest, ev: NextFetchEvent) {
+    if (req.ua?.isBot) {
+        return new Response("no bot in this area");
+    }
+
+    //api 접속을 제외하고 페이지 이동등의 상황에서 쿠키를 확인하여 세션이 없는 경우 enter 페이지로 이동할 수 있도록 미들웨어 구성
+    if(!req.url.includes("/api")) {
+        if (!req.url.includes("/enter") && !req.cookies.carrotSession) {
+            return NextResponse.redirect(`${req.nextUrl.origin}/enter`);
+        }
+    }
+}
