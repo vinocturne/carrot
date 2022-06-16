4713167a3614ff627493a8426b934625623a1a80
Ghunhee Shin <hago@rocketstaff.com>
2022-06-16 12:49:59 +0900

    Fix(mailgun smtp) : "수정 nodemailer-mailgun을 이용한 인증코드 전송"
    nodemailer-mailgun의 smtp를 활용하여 인증코드를 이메일로 전송할 수 있도록 수정

--- libs/server/email.ts
+++ libs/server/email.ts
@@ -1,16 +1,33 @@
 import nodemailer from "nodemailer";
+import mailgun from "nodemailer-mailgun-transport";
 
-const smtpTransport = nodemailer.createTransport({
-    service: "Naver",
-    host: "smtp.naver.com",
-    port: 587,
+// const smtpTransport = nodemailer.createTransport({
+//     service: "Naver",
+//     host: "smtp.naver.com",
+//     port: 587,
+//     auth: {
+//         user: process.env.MAIL_ID,
+//         pass: process.env.MAIL_PASSWORD,
+//     },
+//     tls: {
+//         rejectUnauthorized: false,
+//     },
+// });
+
+interface AuthOptions {
     auth: {
-        user: process.env.MAIL_ID,
-        pass: process.env.MAIL_PASSWORD,
-    },
-    tls: {
-        rejectUnauthorized: false,
-    },
-});
+        api_key: string;
+        domain: string;
+    };
+}
 
-export default smtpTransport;
+const auth: AuthOptions = {
+    auth: {
+        api_key: process.env.MAILGUN_PRIVATE_API!,
+        domain: process.env.MAILGUN_DOMAIN!,
+    },
+};
+
+const nodemailerMailgun = nodemailer.createTransport(mailgun(auth));
+
+export default nodemailerMailgun;
