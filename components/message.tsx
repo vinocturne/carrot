559df5249b1be2948f78d6064a08497fab857aab
Ghunhee Shin <hago@rocketstaff.com>
2022-06-16 15:49:52 +0900

    Fix(Message) : "수정 메세지 프로필 표시"
    메세지에서 프로필이 표시되지 않는 문제 수정

--- components/message.tsx
+++ components/message.tsx
@@ -19,13 +19,17 @@ export default function Message({
                 reversed ? "flex-row-reverse space-x-reverse" : ""
             )}
         >
-            <div className="w-8 h-8 relative rounded-full overflow-hidden bg-slate-400">
-                <Image
-                    alt="avatarUrl"
-                    layout="fill"
-                    src={`https://imagedelivery.net/IfkIh2vCOXio26cf7UQYpw/${avatarUrl}/public`}
-                />
-            </div>
+            {avatarUrl ? (
+                <div className="w-8 h-8 relative rounded-full overflow-hidden bg-slate-400">
+                    <Image
+                        alt="avatarUrl"
+                        layout="fill"
+                        src={`https://imagedelivery.net/IfkIh2vCOXio26cf7UQYpw/${avatarUrl}/public`}
+                    />
+                </div>
+            ) : (
+                <div className="w-8 h-8 rounded-full overflow-hidden bg-slate-400" />
+            )}
             <div className="w-1/2 text-sm text-gray-700 p-2 border border-gray-300 rounded-md">
                 <p>{message}</p>
             </div>
