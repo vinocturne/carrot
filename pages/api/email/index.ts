c2c8f3bbe6bfd79612c5c10c36b68af4ee03282e
Ghunhee Shin <hago@rocketstaff.com>
2022-06-17 15:41:50 +0900

    Feat(mail api) : "추가 mail  웹훅용 api"
    테스트용 메일 웹훅용 api 추가

---
+++ pages/api/email/index.ts
@@ -0,0 +1,15 @@
+import withHandler, { ResponseType } from "@libs/server/withHandler";
+import { NextApiRequest, NextApiResponse } from "next";
+async function handler(
+    req: NextApiRequest,
+    res: NextApiResponse<ResponseType>
+) {
+    console.log("-------------------");
+    console.log("webhook come here");
+    console.log("req");
+    console.log(req);
+    return res.json({
+        ok: true,
+    });
+}
+export default withHandler({ methods: ["POST"], handler, isPrivate: false });
