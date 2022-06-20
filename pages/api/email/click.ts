66956ba0c99ef8cfcad228ae468a31194a3ea4df
Ghunhee Shin <hago@rocketstaff.com>
2022-06-20 17:15:24 +0900

    Fix(reqBody) : "수정 req.body"
    req.body의 event-data에서 recipient와 event를 받아올 수 있도록 수정하였습니다.

--- pages/api/email/click.ts
+++ pages/api/email/click.ts
@@ -24,8 +24,8 @@ async function handler(
 ) {
     const { timestamp, token, signature } = req.body.signature;
     const signingKey = process.env.MAILGUN_WEBHOOK_SIGNING_KEY!;
-    const email = req.body.recipient;
-    const event = req.body.event;
+    const email = req.body["event-data"].recipient;
+    const event = req.body["event-data"].event;
     console.log(req.body);
     console.log(event);
     const isVerified = verify({ signingKey, timestamp, token, signature });
