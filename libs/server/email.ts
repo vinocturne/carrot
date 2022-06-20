62baf1e1707af5ce779ab724c6c711f7e5091d33
Ghunhee Shin <hago@rocketstaff.com>
2022-06-20 12:13:22 +0900

    Fix(tracking) : "수정 tracking 관련 헤더 수정"
    트래킹관련 헤더를 수정하였습니다.

--- libs/server/email.ts
+++ libs/server/email.ts
@@ -1,16 +1,32 @@
 import nodemailer from "nodemailer";
 import mailgun from "nodemailer-mailgun-transport";
 interface AuthOptions {
+    secure: boolean;
     auth: {
         api_key: string;
         domain: string;
+        tracking: string;
+        tracking_clicks: string;
+        tracking_opens: string;
+    };
+    tls: {
+        servername: string;
+        rejectUnauthorized: boolean;
     };
 }
 
 const auth: AuthOptions = {
+    secure: false,
     auth: {
         api_key: process.env.MAILGUN_PRIVATE_API!,
         domain: process.env.MAILGUN_DOMAIN!,
+        tracking: "yes",
+        tracking_clicks: "htmlonly",
+        tracking_opens: "yes",
+    },
+    tls: {
+        servername: "mg.mangakiss.jp",
+        rejectUnauthorized: false,
     },
 };
 const nodemailerMailgun = nodemailer.createTransport(mailgun(auth));
