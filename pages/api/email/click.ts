36f3ebc4228f9721f520d5589b6c46e1910774a6
Ghunhee Shin <hago@rocketstaff.com>
2022-06-20 17:27:42 +0900

    Fix(eventName)

--- pages/api/email/click.ts
+++ pages/api/email/click.ts
@@ -25,10 +25,7 @@ async function handler(
     const { timestamp, token, signature } = req.body.signature;
     const signingKey = process.env.MAILGUN_WEBHOOK_SIGNING_KEY!;
     const email = req.body["event-data"].recipient;
-    const event = req.body["event-data"].event.replace(
-        /^[a-z]/,
-        (char: string) => char.toUpperCase()
-    );
+    const event = req.body["event-data"].event;
     console.log(req.body);
     console.log(event);
     const isVerified = verify({ signingKey, timestamp, token, signature });
